322bbabdddc1321e0346805cea058582f8d5a507
xyz11 <xyz11@192.168.0.6>
2025-06-01 22:49:08 +0200

    test

---
+++ testJson/src/main/java/testJson/testJson/App.java
@@ -0,0 +1,13 @@
+package testJson.testJson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
